319b52b791b0d226bf466f897b58e97aed5b01ae
prokosim <prokosim@gmail.com>
2021-07-27 00:19:16 +0200

    #18 Slow rotation on O, fast on P prototype

---
+++ Showroom/Assets/Scripts/DispatchManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DispatchManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.O))
+        {
+            //too fast
+            //this.transform.Rotate(Vector3.up, 120);
+
+            this.transform.Rotate(Vector3.up, 10);
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            //too fast
+            //this.transform.Rotate(Vector3.up, 120);
+
+            this.transform.Rotate(Vector3.up, Mathf.Lerp(0f, 120f, 20f * Time.deltaTime));
+        }
+
+
+    }
+}
