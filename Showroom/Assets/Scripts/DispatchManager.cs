b9cb5cecce875de3b37972c5fd6570baa6d67fb3
prokosim <kurilluk@email.cz>
2021-07-27 10:09:49 +0200

    #18 Coroutine not working yet...

--- Showroom/Assets/Scripts/DispatchManager.cs
+++ Showroom/Assets/Scripts/DispatchManager.cs
@@ -18,17 +18,29 @@ public class DispatchManager : MonoBehaviour
             //too fast
             //this.transform.Rotate(Vector3.up, 120);
 
-            this.transform.Rotate(Vector3.up, 10);
+            this.transform.Rotate(Vector3.up, -1);
         }
 
         if (Input.GetKeyDown(KeyCode.P))
         {
             //too fast
             //this.transform.Rotate(Vector3.up, 120);
+            StartCoroutine("RotateDispatch");
 
-            this.transform.Rotate(Vector3.up, Mathf.Lerp(0f, 120f, 20f * Time.deltaTime));
+            
+            //this.transform.Rotate(Vector3.up, Mathf.Lerp(0f, 120f, t));
+            
         }
 
+        //It's not working, the coroutine couldn't be started
+        IEnumerator RotateDispatch()
+        {
+            float t = 0f;
+            Debug.Log(t);
+            this.transform.Rotate(Vector3.up, Mathf.Lerp(0f, 120f, t));
+            t += 0.2f;
+            yield return new WaitForSeconds(0.2f);
+        }
 
     }
 }
