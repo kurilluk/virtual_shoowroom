d542edfea1d9e66d05b5f92e6df9e51eee9e297e
Šimon <prokosim@gmail.com>
2021-08-18 14:48:10 +0200

    #14 MENU v main
    Menu in main, with clicking and TAB

--- Showroom/Assets/Scripts/MouseLook.cs
+++ Showroom/Assets/Scripts/MouseLook.cs
@@ -8,7 +8,11 @@ public class MouseLook : MonoBehaviour
     //public GameObject sensitivity;
     public float mouseSensitivity = 100f;
     public Transform playerBody;
+    [SerializeField] public float croachDepth = 1f;
+    [SerializeField] public bool croaching = false;
 
+    public Transform cameraTransform;
+    public Transform mainMenu;
     //bool isMenuOpen = true;
 
     float xRotation = 20f;
@@ -17,12 +21,34 @@ public class MouseLook : MonoBehaviour
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
-
+        cameraTransform = transform;
     }
 
     // Update is called once per frame
-    void Update() 
-    { 
+    void Update()
+    {
+        if (mainMenu.GetChild(0).gameObject.activeSelf == false)
+        {
+            
+        }
+        LookAround();
+        //if (Input.GetKey(KeyCode.LeftControl) && Input.GetKey(KeyCode.RightControl))
+        //if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
+        //{
+        //    Debug.Log("Control pressed");
+
+        //    cameraTransform.position = new Vector3 (transform.position.x, transform.position.y - croachDepth, transform.position.z);
+        //    Debug.Log(cameraTransform.position.y);
+        //}
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            Croach();
+        }
+
+    }
+
+    void LookAround()
+    {
         //TODO: CHeck mouse sensitivity VS pixel size of screen (HiDPI problem)
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
@@ -30,8 +56,23 @@ public class MouseLook : MonoBehaviour
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        
+
         playerBody.Rotate(Vector3.up * mouseX);
+    }
+
+    void Croach()
+    {
+
+        if (!croaching)
+        {
+            croaching = true;
+            cameraTransform.position = new Vector3(transform.position.x, transform.position.y - croachDepth, transform.position.z);
+        }
+        else
+        {
+            croaching = false;
+            cameraTransform.position = new Vector3(transform.position.x, transform.position.y + croachDepth, transform.position.z);
+        }
 
     }
 }
---
+++ Showroom/Assets/Scripts/SelectionManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionManager : MonoBehaviour
+{
+    [SerializeField] public Material highlightedMaterial;
+    [SerializeField] public Material defaultMaterial;
+    //[SerializeField] public Color highlightedColor;
+    //[SerializeField] public Color defaultColor;
+    [SerializeField] public string selectableTag = "Selectable";
+
+    [SerializeField] private LayerMask layerMask;
+
+    private Transform activeSelection;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (activeSelection != null) 
+        {
+            var selectionRenderer = activeSelection.GetComponent<Renderer>();
+            //selectionRenderer.material.color = defaultColor;
+            selectionRenderer.material = defaultMaterial;
+            Debug.Log("The material changed to Default." + defaultMaterial.ToString());
+            activeSelection = null;
+        }
+
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo, Mathf.Infinity, layerMask))
+        {
+            var selection = hitInfo.transform;
+
+            var selectionRenderer = selection.GetComponent<Renderer>();
+            if (selectionRenderer != null)
+            {
+                selectionRenderer.material = highlightedMaterial;
+                Debug.Log("Material changed to Highlighted." + highlightedMaterial.ToString());
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    Debug.Log("Click to enable the Pop-up window now!");
+                    if (selection.GetChild(0).gameObject.activeSelf == false)
+                    {
+                        selection.GetChild(0).gameObject.SetActive(true);
+                    }
+                    else
+                    {
+                        selection.GetChild(0).gameObject.SetActive(false);
+                    }
+                }
+            }
+            activeSelection = selection;
+ 
+
+        }
+    }
+}
