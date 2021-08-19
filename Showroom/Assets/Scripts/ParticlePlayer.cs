d4ecce4d3083118e7ab03d4cac53fb171987d448
Šimon <prokosim@gmail.com>
2021-08-19 17:36:24 +0200

    Create ParticlePlayer.cs

---
+++ Showroom/Assets/Scripts/ParticlePlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticlePlayer : MonoBehaviour
+{
+    [SerializeField] 
+    ParticleSystem birthParticles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            
+            PlayParticles();
+        }
+    }
+
+    public void PlayParticles()
+    {
+        Debug.Log("Playing the particles... now!");
+        //birthParticles.Play();
+        birthParticles.Play(true);
+    }
+}
