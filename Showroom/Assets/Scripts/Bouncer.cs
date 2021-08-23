0031c74fd60cfb7f9ac275757963a07db0edb22a
Šimon <prokosim@gmail.com>
2021-08-23 13:39:19 +0200

    #12 Bouncing is not the best idea...

--- Showroom/Assets/Scripts/Bouncer.cs
+++ Showroom/Assets/Scripts/Bouncer.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Bouncer : MonoBehaviour
 {
     [SerializeField] private float bounceSpeed = 2f;
+    [SerializeField] float maxSize = 0.21f;
+    [SerializeField] float minSize = 0.05f;
+    [SerializeField] float tolerance = 0.1f;
 
     //private float speed = 1f;
     private Vector3 scaleChange;
@@ -12,7 +15,7 @@ public class Bouncer : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
-        scaleChange = Vector3.one * 0.21f;
+        scaleChange = Vector3.one * maxSize;
     }
 
     // Update is called once per frame
@@ -21,14 +24,14 @@ public class Bouncer : MonoBehaviour
         
         //Debug.Log(transform.localScale.y);
         //Very simple bouncing using LERP on scale of the object transform...
-        if (transform.localScale.y > 0.2f)
+        if (transform.localScale.y > maxSize - tolerance)
         {
-            scaleChange = Vector3.one * 0.05f;
+            scaleChange = Vector3.one * minSize;
         }
-        if (transform.localScale.y < 0.055f)
+        if (transform.localScale.y < minSize + tolerance)
         {
             //scaleChange = -scaleChange;
-            scaleChange = Vector3.one * 0.21f;
+            scaleChange = Vector3.one * maxSize;
         }
         transform.localScale = Vector3.Lerp(transform.localScale, scaleChange, bounceSpeed * Time.deltaTime);
 
