7fd39980882b80e93a7b824d979e463e50df478c
Lukas Kurilla <kurilluk@email.cz>
2021-09-20 17:46:32 +0200

    Many updates
    render update
    materials update
    ...

--- Showroom/Assets/Scripts/MouseLook.cs
+++ Showroom/Assets/Scripts/MouseLook.cs
@@ -16,7 +16,7 @@ public class MouseLook : MonoBehaviour
     public Transform mainMenu;
     //bool isMenuOpen = true;
 
-    float xRotation = 20f;
+    float xRotation = 0f;
 
     // Start is called before the first frame update
     void Start()
