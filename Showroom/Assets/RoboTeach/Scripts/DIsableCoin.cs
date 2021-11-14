38a4823ac024d56763bea2da5288a8aa850136fa
prokosim <prokosim@gmail.com>
2021-11-14 18:50:55 +0100

    #31 Coins now disappear after being touched by the robot tip

--- Showroom/Assets/RoboTeach/Scripts/DIsableCoin.cs
+++ Showroom/Assets/RoboTeach/Scripts/DIsableCoin.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DIsableCoin : MonoBehaviour
+public class DisableCoin : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    void OnCollisionEnter(Collision collision)
     {
-        
+        Debug.Log(collision.collider.name);
+        gameObject.SetActive(false);
     }
 }
