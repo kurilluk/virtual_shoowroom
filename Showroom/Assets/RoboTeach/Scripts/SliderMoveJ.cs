afc02cb6c8060db349bc76e6e3212c0c157012b4
prokosim <prokosim@gmail.com>
2021-11-14 17:38:43 +0100

    #31 Sliders attached

---
+++ Showroom/Assets/RoboTeach/Scripts/SliderMoveJ.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SliderMoveJ : MonoBehaviour
+{
+    public Transform robotTransform;
+
+
+    public Transform robotArm0;
+    public Transform robotArm1;
+    public Transform robotArm2;
+    public Transform robotArm3;
+    public Transform robotArm4;
+    public Transform robotArm5;
+
+    public GameObject slider0;
+    public GameObject slider1;
+    public GameObject slider2;
+    public GameObject slider3;
+    public GameObject slider4;
+    public GameObject slider5;
+
+    public float rotationSpeed = 100f;
+
+
+    public void RotateArm0(float rotAmount)
+    {
+        RotateArm(rotAmount, robotArm0, 1);
+    }
+
+    public void RotateArm1(float rotAmount)
+    {
+        RotateArm(rotAmount, robotArm1, 2);
+    }
+
+    public void RotateArm2(float rotAmount)
+    {
+        RotateArm(rotAmount, robotArm2, 2);
+    }
+
+    public void RotateArm3(float rotAmount)
+    {
+        RotateArm(rotAmount, robotArm3, 0);
+    }
+
+    public void RotateArm4(float rotAmount)
+    {
+        RotateArm(rotAmount, robotArm4, 2);
+    }
+
+    public void RotateArm5(float rotAmount)
+    {
+        RotateArm(rotAmount, robotArm5, 0);
+    }
+
+    public void RotateArm(float rotAmount, Transform currentArm, int whichAxis)
+    {
+        float rot = rotAmount * rotationSpeed * Time.deltaTime;
+        //Debug.Log(rot);
+        switch (whichAxis)
+        {
+            case 0:
+                currentArm.Rotate(rot, 0f, 0f, Space.Self);
+                break;
+            case 1:
+                currentArm.Rotate(0f, rot, 0f, Space.Self);
+                break;
+            case 2:
+                currentArm.Rotate(0f, 0f, rot, Space.Self);
+                break;
+            default:
+                break;
+        }
+    }
+}
