3f9e56318ddc0c1703d654cb57eafea4e5816eae
prokosim <prokosim@gmail.com>
2021-11-14 18:28:59 +0100

    #31 Using SetRotation is not a Good idea.... :D But it mooooves!

--- Showroom/Assets/RoboTeach/Scripts/SliderMoveJ.cs
+++ Showroom/Assets/RoboTeach/Scripts/SliderMoveJ.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SliderMoveJ : MonoBehaviour
 {
@@ -14,15 +15,68 @@ public class SliderMoveJ : MonoBehaviour
     public Transform robotArm4;
     public Transform robotArm5;
 
-    public GameObject slider0;
-    public GameObject slider1;
-    public GameObject slider2;
-    public GameObject slider3;
-    public GameObject slider4;
-    public GameObject slider5;
+    //public GameObject slider0;
+    //public GameObject slider1;
+    //public GameObject slider2;
+    //public GameObject slider3;
+    //public GameObject slider4;
+    //public GameObject slider5;
 
     public float rotationSpeed = 100f;
 
+    public Slider slider0;
+    public Slider slider1;
+    public Slider slider2;
+    public Slider slider3;
+    public Slider slider4;
+    public Slider slider5;
+
+    public float rotMin = -180;
+    public float rotMax = 180;
+
+    public void Start()
+    {
+        //Adds a listener to the main slider and invokes a method when the value changes.
+        slider0.onValueChanged.AddListener(delegate { CheckSlider0(); });
+        slider1.onValueChanged.AddListener(delegate { CheckSlider1(); });
+    }
+
+    // Invoked when the value of the slider changes.
+    public void CheckSlider0()
+    {
+        Debug.Log(slider0.value);
+        float sliderValue;
+        sliderValue = slider0.value;
+        sliderValue = Remap(sliderValue, 0, 1, rotMin, rotMax);
+        Debug.Log(sliderValue);
+        RotateArm0(sliderValue);
+    }
+
+    public void CheckSlider1()
+    {
+        Debug.Log(slider1.value);
+        float sliderValue;
+        sliderValue = slider1.value;
+        sliderValue = Remap(sliderValue, 0, 1, rotMin, rotMax);
+        Debug.Log(sliderValue);
+        RotateArm1(sliderValue);
+    }
+
+
+    public float Remap(float from, float fromMin, float fromMax, float toMin, float toMax)
+    {
+        var fromAbs = from - fromMin;
+        var fromMaxAbs = fromMax - fromMin;
+
+        var normal = fromAbs / fromMaxAbs;
+
+        var toMaxAbs = toMax - toMin;
+        var toAbs = toMaxAbs * normal;
+
+        var to = toAbs + toMin;
+
+        return to;
+    }
 
     public void RotateArm0(float rotAmount)
     {
@@ -56,21 +110,40 @@ public class SliderMoveJ : MonoBehaviour
 
     public void RotateArm(float rotAmount, Transform currentArm, int whichAxis)
     {
-        float rot = rotAmount * rotationSpeed * Time.deltaTime;
+        //float rot = rotAmount * rotationSpeed * Time.deltaTime;
+        float rot = rotAmount * rotationSpeed;
         //Debug.Log(rot);
         switch (whichAxis)
         {
             case 0:
-                currentArm.Rotate(rot, 0f, 0f, Space.Self);
+                //currentArm.Rotate(rot, 0f, 0f, Space.Self); 
+                currentArm.SetPositionAndRotation(currentArm.position, Quaternion.Euler(new Vector3(rot, 0f, 0f)));
                 break;
             case 1:
-                currentArm.Rotate(0f, rot, 0f, Space.Self);
+                //currentArm.Rotate(0f, rot, 0f, Space.Self);
+                currentArm.SetPositionAndRotation(currentArm.position, Quaternion.Euler(new Vector3(0f, rot, 0f)));
                 break;
             case 2:
-                currentArm.Rotate(0f, 0f, rot, Space.Self);
+                //currentArm.Rotate(0f, 0f, rot, Space.Self);
+                currentArm.SetPositionAndRotation(currentArm.position, Quaternion.Euler(new Vector3(0f, 0f, rot)));
                 break;
             default:
                 break;
         }
     }
+
+    //public float Remap(this float from, float fromMin, float fromMax, float toMin, float toMax)
+    //{
+    //    var fromAbs = from - fromMin;
+    //    var fromMaxAbs = fromMax - fromMin;
+
+    //    var normal = fromAbs / fromMaxAbs;
+
+    //    var toMaxAbs = toMax - toMin;
+    //    var toAbs = toMaxAbs * normal;
+
+    //    var to = toAbs + toMin;
+
+    //    return to;
+    //}
 }
