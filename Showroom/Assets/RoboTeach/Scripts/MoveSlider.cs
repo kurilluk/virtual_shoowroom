a5ef730ae80d1c8b0b7e7886b75737bd943e826a
prokosim <prokosim@gmail.com>
2021-12-21 22:21:18 +0100

    #31 MoveSlider is a better name

--- Showroom/Assets/RoboTeach/Scripts/MoveSlider.cs
+++ Showroom/Assets/RoboTeach/Scripts/MoveSlider.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
-public class UpdateSlider : MonoBehaviour
+public class MoveSlider : MonoBehaviour
 {
+    public Slider slider;
+    public Button buttonMinus;
+    public Button buttonPlus;
+    public float sliderStep = 5f;
+
+    public TextMeshPro sliderText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        sliderText = FindObjectOfType<TextMeshPro>();
+
+        buttonMinus.onClick.AddListener(delegate { DecreaseSlider(); });
+        buttonPlus.onClick.AddListener(delegate { IncreaseSlider(); });
     }
 
-    // Update is called once per frame
-    void Update()
+    void DecreaseSlider() 
     {
-        
+        slider.value -= sliderStep;
+    }
+    void IncreaseSlider() 
+    {
+        slider.value += sliderStep;
     }
 }
